96f66a5e195f64982c7b13a327743bc347226ab6
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-03-15 23:44:15 -0300

    mui

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,120 @@
+import { Fragment } from "react";
+import { Avatar, AvatarGroup, Box, Divider, ImageList, ImageListItem, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+
+export default function Rightbar() {
+  return (
+    <Box
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block", } }}
+    >
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
+        <AvatarGroup max={7}>
+          <Avatar alt="Remy Sharp" src="https://mui.com/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="" />
+          <Avatar alt="Trevor Henderson" src="https://mui.com/static/images/avatar/5.jpg" />
+          <Avatar alt="Remy Sharp" src="https://mui.com/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="https://mui.com/static/images/avatar/4.jpg" />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} my={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://mui.com/static/images/image-list/breakfast.jpg"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://mui.com/static/images/image-list/burgers.jpg"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://mui.com/static/images/image-list/camera.jpg"
+              alt=""
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2}>
+          Latest Conversations
+        </Typography>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://mui.com/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="https://mui.com/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {' — Do you have Paris recommendations? Have you ever…'}
+                </Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  )
+}
